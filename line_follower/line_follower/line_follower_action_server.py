4c0ac6561410683b3306c0125403df0adb0e0197
bingxue-xu <autoxue@gmail.com>
2025-05-19 11:05:49 +0200

    too slow

--- line_follower/line_follower/line_follower_action_server.py
+++ line_follower/line_follower/line_follower_action_server.py
@@ -30,10 +30,9 @@ class LineFollowerActionServer(Node):
             10
         )
 
-        self.create_subscription(Image, '/seen_image', self._image_callback, 10)
-        self.create_subscription(Image, '/camera/camera/color/image_raw', self._image_callback, 10)
+        self.create_subscription(Image, '/map', self._map_callback, 10)
 
-        self.robot_pos_pixel = None
+        self.robot_pos_pixel = 1442, 3180
         self.pts = None
 
     def robot_pos_callback(self, msg: PointPixel):
@@ -41,7 +40,7 @@ class LineFollowerActionServer(Node):
         self.get_logger().info(f'Robot position: {msg.width}, {msg.height}')
 
 
-    def _image_callback(self, msg: Image):
+    def _map_callback(self, msg: Image):
         image = self.bridge.imgmsg_to_cv2((msg, 'rgb8'))
         hsv_image = cv2.cvtVolor(image, cv2.COLOR_RGB2HSV)
         mask = cv2.inRange(hsv_image, (0, 100, 100), (30, 2555, 255))  
@@ -54,50 +53,54 @@ class LineFollowerActionServer(Node):
 
     def execute_callback(self, goal_handle):
         start_time = self.get_clock().now().nanoseconds * 1e-9
+        elapsed = 0.0
         move_flag = LineFollower.Goal()
         feedback_msg = LineFollower.Feedback()
         result = LineFollower.Result()
 
-        try:
-            while move_flag.flag:
-                self.get_logger().info('Clara, färdiga, gå...')
-                cur_time = self.get_clock().now().nanoseconds * 1e-9
-                elapsed = cur_time - start_time
+        if move_flag.flag:
+            cur_time = self.get_clock().now().nanoseconds * 1e-9
+            elapsed = cur_time - start_time
+            self.get_logger().info('Clara, färdiga, gå...')
 
+        try:
+            if elapsed < 10.0:
                 if goal_handle.is_cancel_requested:
-                    self.get_logger().info('Goal cancelled')
                     goal_handle.canceled()
                     result.result = False
+                    self.get_logger().info('Goal cancelled')
                     return result
 
                 if not self.umpire():
                     feedback_msg.status = 'Out of line :('
                     goal_handle.publish_feedback(feedback_msg)
-                    result.result = False
                     goal_handle.succeed()
+                    result.result = False
                     return result
 
                 feedback_msg.status =  f'Running, elapsed={elapsed:.1f}s'
                 goal_handle.publish_feedback(feedback_msg)
 
-                if elapsed > 10.0:
-                    self.get_logger().info('Time is up')
-                    result.result = True
-                    goal_handle.succeed()
-                    return result
+            goal_handle.succeed()
+            result.result = True
+            self.get_logger().info('Time is up')
+            return result
                 
         except Exception as e:
             self.get_logger().error(f'Error: {e}')
             goal_handle.abort()
             return result
     
+
     def umpire(self)-> bool:
+        """
+        check wether the robot is within workspace and not devating the line 
+        """
 
         if self.robot_pos_pixel is None:
             self.get_logger().error('Robot position not available')
             return False
         
-        self.robot_pos_pixel = 1442, 3180
         robot_column, robot_row = self.robot_pos_pixel
 
         self.get_logger.info('Robot position in image: {robot_column}, {robot_row}')
@@ -109,7 +112,7 @@ class LineFollowerActionServer(Node):
         min_y, max_y = -inflation, workspace_height + inflation
 
         if not (min_x <= robot_column <= max_x and min_y <= max_y):
-            self.get_logger().error('out of bounds :')
+            self.get_logger().error('out of bounds :(')
             return False
         else:
             if self.pts is not None:
