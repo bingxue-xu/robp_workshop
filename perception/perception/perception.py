38232b53503aa479585b3001059dcb168e95019a
Daniel Duberg <danielduberg@gmail.com>
2025-05-20 13:58:09 +0200

    Formatted

--- perception/perception/perception.py
+++ perception/perception/perception.py
@@ -7,16 +7,19 @@ import cv2
 import numpy as np
 from robp_interfaces.msg import PointPixel, HSVFilter
 
+
 class Perception(Node):
     def __init__(self):
         super().__init__('perception')
         self.bridge = CvBridge()
 
         self.create_subscription(Image, '/seen_image', self.image_callback, 10)
-        self.create_subscription(Image, '/camera/camera/color/image_raw', self.image_callback, 10)
+        self.create_subscription(
+            Image, '/camera/camera/color/image_raw', self.image_callback, 10)
 
-        self.point_pub = self.create_publisher(PointPixel,'/found_point', 10)
-        self.hsv_filter_pub = self.create_publisher(HSVFilter,'/hsv_filter', 10)
+        self.point_pub = self.create_publisher(PointPixel, '/found_point', 10)
+        self.hsv_filter_pub = self.create_publisher(
+            HSVFilter, '/hsv_filter', 10)
 
     def find_point_on_line(self, msg: Image):
         """
@@ -38,17 +41,18 @@ class Perception(Node):
 
         hsv_filter = HSVFilter()
         ############## START Modify the values inbetween here ################
-        hsv_filter.hue_min = 40
-        hsv_filter.hue_max = 60 # Max is 360
-        hsv_filter.saturation_min = 100
-        hsv_filter.saturation_max = 255
-        hsv_filter.value_min = 0
-        hsv_filter.value_max = 255
+        hsv_filter.hue_min = 0   # Range: [0, 360]
+        hsv_filter.hue_max = 360  # Range: [0, 360]
+        hsv_filter.saturation_min = 0  # Range: [0, 255]
+        hsv_filter.saturation_max = 255  # Range: [0, 255]
+        hsv_filter.value_min = 0  # Range: [0, 255]
+        hsv_filter.value_max = 255  # Range: [0, 255]
         ############## END Modify the values inbetween here ################
-        self.hsv_filter_pub.publish(hsv_filter) 
+        self.hsv_filter_pub.publish(hsv_filter)
 
         # Remove pixles outside the values above from the image
-        tape_mask = cv2.inRange(hsv_image, (hsv_filter.hue_min / 2, hsv_filter.saturation_min, hsv_filter.value_min), (hsv_filter.hue_max / 2, hsv_filter.saturation_max, hsv_filter.value_max))
+        tape_mask = cv2.inRange(hsv_image, (hsv_filter.hue_min / 2, hsv_filter.saturation_min,
+                                hsv_filter.value_min), (hsv_filter.hue_max / 2, hsv_filter.saturation_max, hsv_filter.value_max))
 
         ############## START Improve/change below ###########################
         for row in range(height):
@@ -56,18 +60,16 @@ class Perception(Node):
                 if tape_mask[row, column]:
                     line_column = column
                     line_row = row
-                    return line_column, line_row
         ############## END Improve/change below ###########################
 
         return line_column, line_row
 
     def image_callback(self, msg: Image):
-
         """
         Find the target point and publish it.
         :param msg: The image message
         :return: Point pixel values (column, row) 
-        
+
         """
         column, row = self.find_point_on_line(msg)
 
@@ -79,7 +81,6 @@ class Perception(Node):
         pt.column = column
         pt.row = row
         self.point_pub.publish(pt)
-        self.get_logger().debug(f'Publishing point on image msg {column, row}')
 
 
 def main():
@@ -89,5 +90,6 @@ def main():
     node.destroy_node()
     rclpy.shutdown()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
