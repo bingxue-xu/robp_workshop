eb64a5419c4a7bb61e9627fa6162fdd0b7535b4f
Daniel Duberg <danielduberg@gmail.com>
2025-05-20 15:52:33 +0200

    Scripts to make running easier

---
+++ simulator.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# this forces bash to use the Desktop directory i.e. where this bash script is located
+cd "${0%/*}"
+
+source /opt/ros/jazzy/setup.bash
+source ../install/local_setup.bash
+ros2 launch robp_boot_camp_launch workshop_sim_launch.xml
---
+++ upload.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# this forces bash to use the Desktop directory i.e. where this bash script is located
+cd "${0%/*}"
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+group_name=""
+
+while [ "$group_name" = "" ]
+do 
+	read -p "Enter group name: " group_name
+done
+
+echo -e "${GREEN}Uploading solution to robot${NC}"
+
+scp /home/robot/workshop_ws/src/perception/perception/perception.py robot@192.168.128.108:~/${group_name}/perception.py
+
+if [ "$?" != "0" ]
+then
+	echo -e "${RED}Could not connect to the robot${NC}"
+	# sleep 10
+	# exit $?
+fi
+
+scp /home/robot/workshop_ws/src/controller/controller/controller.py robot@192.168.128.108:~/${group_name}/controller.py
+
+if [ "$?" != "0" ]
+then
+	echo -e "${RED}Could not connect to the robot${NC}"
+	# sleep 10
+	# exit $?
+fi
+
+echo -e "${GREEN}Solution has been uploaded to robot as group ${group_name}${NC}"
+
+sleep 10
