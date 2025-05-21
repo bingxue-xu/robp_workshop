c54bc97d9be5aa92848e1912f74336eac1f13c24
danielduberg <danielduberg@gmail.com>
2025-05-21 11:12:09 +0200

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -16,8 +16,8 @@ done
 
 echo -e "${GREEN}Uploading group '${group_name}' solution to robot${NC}"
 
-sshpass -p "ros2" ssh -o StrictHostKeyChecking=no rosuser@192.168.128.108 "mkdir -p ~/${group_name}"
-sshpass -p "ros2" scp /home/robot/workshop_ws/src/perception/perception/perception.py rosuser@192.168.128.108:~/${group_name}/perception.py
+sshpass -p "ros2" ssh -o StrictHostKeyChecking=no rosuser@192.168.128.108 "mkdir -p ~/workshop/${group_name}"
+sshpass -p "ros2" scp /home/robot/workshop_ws/src/perception/perception/perception.py rosuser@192.168.128.108:~/workshop/${group_name}/perception.py
 
 if [ "$?" != "0" ]
 then
@@ -26,7 +26,7 @@ then
 	exit $?
 fi
 
-sshpass -p "ros2" scp /home/robot/workshop_ws/src/controller/controller/controller.py rosuser@192.168.128.108:~/${group_name}/controller.py
+sshpass -p "ros2" scp /home/robot/workshop_ws/src/controller/controller/controller.py rosuser@192.168.128.108:~/workshop/${group_name}/controller.py
 
 if [ "$?" != "0" ]
 then
