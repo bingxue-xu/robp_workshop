c777b6504271986f7471ca2578b9a4c423c78a38
Daniel Duberg <danielduberg@gmail.com>
2025-05-21 08:34:28 +0200

    Fixed so students can see output from their nodes in terminal

--- simulator.sh
+++ simulator.sh
@@ -6,4 +6,4 @@ cd "${0%/*}"
 source /opt/ros/jazzy/setup.bash
 source ../install/local_setup.bash
 export ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST
-ros2 launch robp_boot_camp_launch workshop_sim_launch.xml
+(trap 'kill 0' SIGINT; ros2 launch robp_boot_camp_launch workshop_student_launch.xml & ros2 launch robp_boot_camp_launch workshop_sim_launch.xml >/dev/null)
